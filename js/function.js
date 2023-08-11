99ac8166c1102e458549a4a781a8cab4a771ca17
CatTheMeow <naenggi19@gmail.com>
2023-08-11 17:26:01 +0900

    23_08_11

--- js/function.js
+++ js/function.js
@@ -1,27 +1,33 @@
-// 로딩중
+//로딩중
 $(function () {
-	const $loading = $('div.loading');
+	const $loading = $('.loading');
 	$loading.children('p').fadeOut();
 	$loading.delay(250).fadeOut(800);
 });
 
 //
 $(function () {
+	const $h1 = $('h1');
 	const $home = $('#home');
-	/*
-	window = 브라우저
-	innerHeight = 요소안의 높이
+	const $intro = $home.children('.intro');
 
-	브라우저 화면의 크기
-	1. 스크롤바, 불바를 제외한 화면의 크기
-		window.innerWidth
-		window.innerHeight
-		
-	2. 스크롤바, 불바를 포합한크기
-		window.outerWidth
-		window.outerHeight	
-	*/
 	$(window).on('load resize', function () {
+		/*
+		  브라우저 화면의 크기
+	
+		  1) 스크롤바와 툴바를 포함하지 않은 브라우저 화면의 크기
+			 window.innerWidth
+			 window.innerHeight
+	
+		  2) 스크롤바와 툴바를 포함한 브라우저 화면의 크기
+			 window.outerWidth
+			 window.outerHeight
+	   */
 		$home.height(window.innerHeight);
+
+		$h1.css({
+			//선택된 요소가 body로부터 이르는 거리 (left, top)
+			top: $intro.offset().top - 72,
+		});
 	});
 });
